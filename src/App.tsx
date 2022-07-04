47f37b1ff8d405a73d4b7a11634d7bde14b86423
Hans Burbano <hburbano@launchmobility.com>
2022-07-04 10:21:10 -0500

    Add SWR and fetch devices hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import './App.css'
-import { DeviceList, useDevices } from './components'
+import useSWR, { SWRConfig } from 'swr'
+import { Routes, Route } from 'react-router-dom'
+import { DeviceList, DeviceEditor, useDevices } from './components'
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 function App() {
-  const deviceHooks = useDevices()
-
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <DeviceList {...deviceHooks} />
+      <SWRConfig
+        value={{
+          // refreshInterval: 3000,
+          fetcher,
+        }}
+      >
+        <Routes>
+          <Route index element={<DeviceList />} />
+          <Route path="/devices/:deviceId" element={<DeviceEditor />} />
+          <Route path="/devices/create" element={<DeviceEditor />} />
+          <Route path="*" element={<DeviceList />} />
+        </Routes>
+      </SWRConfig>
     </div>
   )
 }
--- src/components/hooks.ts
+++ src/components/hooks.ts
@@ -1,12 +1,27 @@
-import useSWR from 'swr'
+import useSWR, { mutate } from 'swr'
 
 //Will use .env on prod
 const BASE_URL = 'http://localhost:3000'
 
-const fetcher = (url: string) => fetch(url).then((r) => r.json())
-
 const useDevices = () => {
-  const { data, error, mutate } = useSWR(`${BASE_URL}/devices`, fetcher)
+  const { data, error } = useSWR<Device[]>(`${BASE_URL}/devices`)
+
+  const onDelete = async (device_id: string) => {
+    try {
+      await fetch(`${BASE_URL}/devices/${device_id}`, {
+        method: 'DELETE',
+      })
+      await mutate(`${BASE_URL}/devices`)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return { devices: data || [], error, isLoading: !data, onDelete }
+}
+
+const useDevice = (deviceId?: string) => {
+  const { data, error } = useSWR<Device>(deviceId ? `${BASE_URL}/devices/${deviceId}` : null)
 
   const onCreate = async (device: Device) => {
     try {
@@ -17,7 +32,7 @@ const useDevices = () => {
           'Content-Type': 'application/json',
         },
       })
-      await mutate()
+      await mutate(`${BASE_URL}/devices`)
     } catch (err) {
       console.error(err)
     }
@@ -32,24 +47,20 @@ const useDevices = () => {
           'Content-Type': 'application/json',
         },
       })
-      await mutate()
+      await mutate([`${BASE_URL}/devices/`, device.id])
+      await mutate(`${BASE_URL}/devices`)
     } catch (err) {
       console.error(err)
     }
   }
 
-  const onDelete = async (device_id: string) => {
-    try {
-      await fetch(`${BASE_URL}/devices/${device_id}`, {
-        method: 'DELETE',
-      })
-      await mutate()
-    } catch (err) {
-      console.error(err)
-    }
+  return {
+    device: data || { system_name: '', hdd_capacity: '', type: '' },
+    error,
+    isLoading: !data,
+    onUpdate,
+    onCreate,
   }
-
-  return { devices: data || [], error, isLoading: !data, onCreate, onUpdate, onDelete }
 }
 
-export { useDevices }
+export { useDevice, useDevices }
